95ba302d4691e86e29049334920508c4a7a8f667
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-05-12 08:34:50 +0300

    dbQueue added

---
+++ app/offline/dbQueue.ts
@@ -0,0 +1,7 @@
+let queue = Promise.resolve();
+
+export function runDbTask<T>(task: () => Promise<T>): Promise<T> {
+  const next = queue.then(task, task);
+  queue = next.then(() => undefined, () => undefined);
+  return next;
+}
