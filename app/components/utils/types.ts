21b959f93f7bf27656925ce0a8500235ddfa3b4d
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-07-12 11:43:38 +0300

    asset modal

--- app/components/utils/types.ts
+++ app/components/utils/types.ts
@@ -1,14 +1,16 @@
 import { AssetItem } from "../../../api/api";
 
-//components/utils/types.ts
+// components/utils/types.ts
+
 export type UploadFileInput = {
   uri: string;
   name: string;
   type: string;
 };
 
-export type AssetCondition = "" | "New" | "Used" | "Damaged" | "Good";
-export type AssetType = "vehicle" | "other"  ;
+export type AssetCondition = string | null;
+
+export type AssetType = "vehicle" | "other";
 
 export type AssetMediaInput = {
   uri?: string;
@@ -18,25 +20,37 @@ export type AssetMediaInput = {
   publicId?: string | null;
   duration?: number | null;
   existing?: boolean;
+
+  // for video/image support
+  mediaType?: "image" | "video";
+  mimeType?: string | null;
+  thumbnailUrl?: string | null;
 };
 
 export type AssetDraft = {
   images: AssetMediaInput[];
   name: string;
   writtenDescription: string;
+
   code?: string;
   rawData?: Record<string, any> | null;
+
   voiceNotes: AssetMediaInput[];
+
   condition?: AssetCondition;
+
   assetType?: AssetType;
   quantity?: number;
   subAssetType?: string;
+
   brand?: string;
   model?: string;
   manufactureYear?: string;
   kilometersDriven?: string;
+
   hasNotes?: boolean;
   notes?: string;
+
   isDone?: boolean;
   isPresent?: boolean;
 };
