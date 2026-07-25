bd5401569e7fccec1c311532bb99220c06ab57bf
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-07-25 08:31:49 +0300

    app/offline/network.ts

--- app/offline/network.ts
+++ app/offline/network.ts
@@ -1,17 +1,33 @@
 
-//offline/network.ts
-import NetInfo from '@react-native-community/netinfo';
-import { useEffect, useState } from 'react';
+// //offline/network.ts
+// import NetInfo from '@react-native-community/netinfo';
+// import { useEffect, useState } from 'react';
 
-export function useIsOnline() {
-  const [isOnline, setIsOnline] = useState<boolean | null>(null);
+// export function useIsOnline() {
+//   const [isOnline, setIsOnline] = useState<boolean | null>(null);
 
-  useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener((state) => {
-      setIsOnline(state.isConnected ?? false);
-    });
-    return unsubscribe;
-  }, []);
+//   useEffect(() => {
+//     const unsubscribe = NetInfo.addEventListener((state) => {
+//       setIsOnline(state.isConnected ?? false);
+//     });
+//     return unsubscribe;
+//   }, []);
 
-  return isOnline ?? true;
+//   return isOnline ?? true;
+// }
+
+// offline/network.ts
+import { useConnectivity } from "../components/connectivity/ConnectivityContext";
+
+export function useIsOnline(): boolean | null {
+  const {
+    initialized,
+    isOnline,
+  } = useConnectivity();
+
+  if (!initialized) {
+    return null;
+  }
+
+  return isOnline;
 }
