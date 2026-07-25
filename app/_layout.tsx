398dfefc1bff726b99891d83adfa9d3f392a5884
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2026-07-25 08:33:45 +0300

    all

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,30 @@
 // app/_layout.tsx
+
 import "./sync/projectBackgroundTask";
 
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
+
 import { AuthProvider } from "../api/AuthContext";
+import { ConnectivityProvider } from "./components/connectivity/ConnectivityContext";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.root}>
-      <AuthProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            animation: "slide_from_right",
-            contentStyle: { backgroundColor: "#0c0c0c" },
-          }}
-        />
-      </AuthProvider>
+      <ConnectivityProvider>
+        <AuthProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              animation: "slide_from_right",
+              contentStyle: {
+                backgroundColor: "#0c0c0c",
+              },
+            }}
+          />
+        </AuthProvider>
+      </ConnectivityProvider>
     </GestureHandlerRootView>
   );
 }
